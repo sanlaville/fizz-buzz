65fa3abe96af3fdfcc1f4a75d3b8684971f670c2
Christophe Maldivi <christophe.maldivi@orange.com>
2019-09-20 22:23:53 +0200

    add kotlin

---
+++ kotlin/README.md
@@ -0,0 +1,10 @@
+[![MIT licensed](https://img.shields.io/badge/license-MIT-blue.svg)](../LICENSE)
+
+# Useful dependencies
+
+ - [AssertJ - Fluent assertions for java](https://joel-costigliola.github.io/assertj/)
+ 
+ - [Mockito - Tasty mocking framework for unit tests in Java](https://site.mockito.org/)
+ 
+# License
+Copyright (C) 2016 Orange - The MIT License
---
+++ kotlin/src/test/kotlin/com/orange/dojo/fizzbuzz/TestEnv.kt
@@ -0,0 +1,27 @@
+package com.orange.dojo.fizzbuzz
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+
+class EnvTest {
+
+    @Test
+    fun `test environment is working`() {
+        // Given
+
+        // When
+
+        // Then
+        assertThat(0).isEqualTo(1)
+    }
+
+    @ParameterizedTest
+    @CsvSource("1, a1", "2, a2")
+    fun `parameterized test is working`(num: Int, exp: String) {
+        val value = "a$num"
+        assertThat(value).isEqualTo(exp)
+    }
+}
