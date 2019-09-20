1326193f38bd500126b7ae998ee531e984ca32f1
Christophe Maldivi <christophe.maldivi@orange.com>
2019-09-20 22:35:46 +0200

    add js

---
+++ js/README.md
@@ -0,0 +1,3 @@
+# Quick start
+
+ - test: open file `test/js/jasmine/SpecRunner.html` in your browser: test should fail by default to test your environment
---
+++ js/test/js/env_spec.js
@@ -0,0 +1,9 @@
+// see https://jasmine.github.io/api/2.8/matchers.html
+
+describe('Check test environment', function () {
+
+    it('test should fail', function () {
+        expect(1).toEqual(0);
+    });
+
+});
